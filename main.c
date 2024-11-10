8ea57f1da46b45dcb1b29b43f0c2e8ad027486b4
Del <matteo.tutti@epitech.eu>
2024-11-10 21:00:35 +0100

    Init of the project and first try of actions

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** main
+** File description:
+** My Test Main
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello my world!");
+    return 84;
+
+}
