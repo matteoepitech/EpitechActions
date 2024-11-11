d9ff3f5c8aedf2e5e6d37b20aea2bb1978bb9af9
Del <matteo.tutti@epitech.eu>
2024-11-11 11:14:56 +0100

    Update install_epitech_actions.sh

--- install_epitech_actions.sh
+++ install_epitech_actions.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+print_message() {
+    # ANSI Colors
+    DARK_BLUE='\033[0;34m'
+    LIGHT_BLUE='\033[0;36m'
+    NC='\033[0m'
+    
+    echo -e "${DARK_BLUE}[${LIGHT_BLUE}EpitechActions${DARK_BLUE}]${NC} $1"
+}
+
 if [ -z "$1" ]; then
-    echo "Please specify the path of your github folder."
-    echo "Usage : ./install_epitech_actions.sh <path>"
+    print_message "Please specify the path of your github folder."
+    print_message "Usage : ./install_epitech_actions.sh <path>"
     exit 1
 fi
 
 REPO_PATH="$1"
-
 if [ ! -d "$REPO_PATH" ]; then
-    echo "This path is not a folder : $REPO_PATH"
-    echo "Did you spell it right ?"
+    print_message "This path is not a folder : $REPO_PATH"
+    print_message "Did you spell it right ?"
     exit 1
 fi
 
-echo "Do you want to install a github protection when pushing ? (y/n)"
+print_message "Do you want to install a github protection when pushing ? (y/n)"
 read response
 
 if [ "$response" = "y" ]; then
     # Is Github CLI is installed
     if ! command -v gh &> /dev/null; then
-        echo "GitHub CLI (gh) is not installed on your device. Installing..."
+        print_message "GitHub CLI (gh) is not installed on your device. Installing..."
         if [ "$(uname)" == "Darwin" ]; then
             brew install gh
         elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
@@ -34,8 +42,8 @@ if [ "$response" = "y" ]; then
 
     # Authentificate with login web browser
     if ! gh auth status &> /dev/null; then
-        echo "Starting the authentification..."
-        echo "A window in your browser will open..."
+        print_message "Starting the authentification..."
+        print_message "A window in your browser will open..."
         gh auth login --web
     fi
 
@@ -45,7 +53,7 @@ if [ "$response" = "y" ]; then
     REPO_NAME=$(echo $REPO_URL | awk -F'[/:]' '{print $NF}' | sed 's/.git$//')
 
     # Configuring the protection
-    echo "Configuring the protection minimal..."
+    print_message "Configuring the protection minimal..."
     gh api \
       --method PUT \
       -H "Accept: application/vnd.github+json" \
@@ -58,21 +66,18 @@ if [ "$response" = "y" ]; then
       -f restrictions=null
 
     if [ $? -eq 0 ]; then
-        echo "Success! Your protection as been installed correctly"
-        echo "Pushes will be blocked only if tests fail."
+        print_message "Success! Your protection as been installed correctly"
+        print_message "Pushes will be blocked only if tests fail."
     else
-        echo "Error! There is an error during the installation, try creating the branch protection manually in the repo's settings."
+        print_message "Error! There is an error during the installation, try creating the branch protection manually in the repo's settings."
     fi
 fi
 
-echo "Starting the download of the github actions..."
+print_message "Starting the download of the github actions..."
 git clone https://github.com/matteoepitech/EpitechActions.git /tmp/epitech-actions-github
-
-echo "Adding the workflow in this github..."
+print_message "Adding the workflow in this github..."
 cp -r /tmp/epitech-actions-github/.github "$REPO_PATH/.github"
-
-echo "Cleaning up the useless files..."
+print_message "Cleaning up the useless files..."
 rm -rf /tmp/epitech-actions-github/
-
-echo "The Epitech Actions Workflows is implemented in $REPO_PATH."
-echo "Sucess!"
+print_message "The Epitech Actions Workflows is implemented in $REPO_PATH."
+print_message "Success!"
