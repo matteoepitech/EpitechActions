4a1ee28c655de0466a75b3719ac7ac4563846766
matteoepitech <matteo.tutti@epitech.eu>
2025-03-19 15:01:15 +0100

    [~] Refractor of the coding style checker

--- install_epitech_actions.sh
+++ install_epitech_actions.sh
@@ -28,5 +28,25 @@ print_message "Adding the workflow in this github..."
 cp -r /tmp/epitech-actions-github/.github "$REPO_PATH/.github"
 print_message "Cleaning up the useless files..."
 rm -rf /tmp/epitech-actions-github/
-print_message "The Epitech Actions Workflows is implemented in $REPO_PATH"
-print_message "Success!"
+print_message "The Epitech Actions Workflows is implemented in ${REPO_PATH}."
+
+
+HOOKS_PATH="$REPO_PATH/.git/hooks"
+HOOK_FILE="$HOOKS_PATH/epitech-actions-post-commit"
+
+print_message "Adding the Git hook to detect conflicts after a commit..."
+cat << EOF > "$HOOK_FILE"
+#!/bin/bash
+if git ls-files -u | grep .; then
+  printf "\033[31;47;1mWarning! This commit has unresolved conflicts...\033[0m\n"
+else
+  printf "\033[32;47;1mCool this commit has no conflicts!\033[0m\n"
+fi
+EOF
+
+chmod +x "$HOOK_FILE"
+print_message "Git hook successfully installed."
+print_message "All hooks installed :"
+print_message "- $HOOK_FILE --> Say if any conflicts or not during commit phase."
+
+print_message "Success!
