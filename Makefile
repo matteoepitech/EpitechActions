4ba1cb1fccb67225886020511b6e9750a9e1b98b
matteoepitech <matteo.tutti@epitech.eu>
2025-05-05 20:46:02 +0200

    [~] Did some change concerning the checking of undefined symbols, and removed tests files

--- Makefile
+++
@@ -1,8 +0,0 @@
-NAME = a.out
-SRC = test.c
-
-all:
-	$(CC) $(SRC) -o $(NAME)
-
-clean:
-	rm -f $(NAME)
--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void) {
-    printf("Hello from printf!\n");
-    write(1, "Hello from write!\n", 18);
-    return 0;
-}
